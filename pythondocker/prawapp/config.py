262f5dff42ea29e1e665f48c7abcfc28a8cd7319
roshan4545 <60842890+roshan4545@users.noreply.github.com>
2021-01-07 20:59:21 +0530

    Delete config.py

--- pythondocker/prawapp/config.py
+++
@@ -1,7 +0,0 @@
-#Enter your own username,password
-#enter the client_id and client_secret which we get while creating the application 
-
-username = ""
-password = ""
-client_id = ""
-client_secret = ""
